e5141a31184634d1f6fdf86e65a0f73c3da10cc4
juandigv69 <juandigv69@gmail.com>
2021-02-01 16:01:49 -0400

    Added basic search components

---
+++ src/views/SearchPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="searchList">
+    <h1> iTunes Search & Selector </h1>
+    <input v-model="searchField" placeholder="Search"/>
+    <select v-model="searchSelect">
+      <option :key="type" v-for="type in this.searchType"> {{type}} </option>
+    </select>
+    <button v-on:click="getSearchResult">
+      Search
+    </button>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'SearchPage',
+  data(){
+    return{
+      searchField: "",
+      searchSelect: "All",
+      searchType:["All","Podcast","Music","AudioBook"],
+      searchResult: "DEFAULT"
+    }
+  },
+  methods: {
+    getSearchResult(){
+      //
+    }
+  }
+}
+</script>
