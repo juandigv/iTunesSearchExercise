96826643329a34d43ef14d6f29be0703d92a93fa
juandigv69 <juandigv69@gmail.com>
2021-02-01 17:07:29 -0400

    Added iTunes search URL generation

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -3,7 +3,7 @@
     <h1> iTunes Search & Selector </h1>
     <input v-model="searchField" placeholder="Search"/>
     <select v-model="searchSelect">
-      <option :key="type" v-for="type in this.searchType"> {{type}} </option>
+      <option v-for="type in this.searchType" :key="type.value" :value="type.value"> {{type.text}} </option>
     </select>
     <button v-on:click="getSearchResult">
       Search
@@ -17,14 +17,37 @@ export default {
   data(){
     return{
       searchField: "",
-      searchSelect: "All",
-      searchType:["All","Podcast","Music","AudioBook"],
-      searchResult: "DEFAULT"
+      searchSelect: "all",
+      searchType:[
+        {text:"All",value:"all"},
+        {text:"Movie",value:"movie"},
+        {text:"Podcast",value:"podcast"},
+        {text:"Music",value:"musictrack"},
+        {text:"Music Video",value:"musicvideo"},
+        {text:"Audio Book",value:"audiobook"},
+        {text:"Shortfilm",value:"shortfilm"},
+        {text:"TV Show",value:"tvshow"},
+        {text:"Software",value:"software"},
+        {text:"E-Book",value:"ebook"},
+        ],
+      searchResult: [],
+      template: {
+        imageCover: "",
+        name: "",
+        artist: "",
+        price: "",
+      },
+      generatedURL: ""
     }
   },
   methods: {
     getSearchResult(){
-      //
+      console.log("Searching for "+ this.searchField +" in "+ this.searchSelect);
+      this.generatedURL = "https://itunes.apple.com/search?term="+this.searchField;
+      if(this.searchSelect != "all"){
+        this.generatedURL+="&media="+this.searchSelect;
+      }
+      console.log("URL: "+this.generatedURL);
     }
   }
 }
