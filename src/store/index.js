59300ff593bc7461628b51d400e27df6942edcec
juandigv69 <juandigv69@gmail.com>
2021-02-01 22:09:12 -0400

    Implemented Vuex states
    Added basic listing method for search

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,29 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
+  state: {
+    searchResults: []
+  },
+  mutations: {
+    addMedia(state, mediaToAdd) {
+      state.searchResults.push(mediaToAdd);
+    },
+    clearSearchResults(state) {
+      state.searchResults.length = 0;
+    }
+  },
+  actions: {
+    addMedia({ commit }, mediaToAdd) {
+      commit("addMedia", mediaToAdd);
+    },
+    clearSearchResults({ commit }) {
+      commit("clearSearchResults");
+    }
+  },
+  getters: {
+    getSearchResults(state){
+      return state.searchResults;
+    }
+  },
   modules: {}
 });
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,59 +1,98 @@
 <template>
-    <div class="searchList">
-        <h1> iTunes Search & Selector </h1>
-        <input v-model="searchField" placeholder="Search" />
-        <select v-model="searchSelect">
-                                          <option v-for="type in this.searchType" :key="type.value" :value="type.value"> {{type.text}} </option>
-                                        </select>
-        <button v-on:click="getSearchResult">
-                                          Search
-                                        </button>
+    <div class="searchPage">
+        <div class="searchBar">
+            <h1>iTunes Search & Selector</h1>
+            <input v-model="searchField" placeholder="Search" />
+            <select v-model="searchSelect">
+                                  <option
+                                    v-for="type in this.searchType"
+                                    :key="type.value"
+                                    :value="type.value"
+                                  >
+                                    {{ type.text }}
+                                  </option>
+                                </select>
+            <button v-on:click="getSearchResult">Search</button>
+        </div>
+        <div class="searchList">
+            <div class="media" v-for="media in searchResults" :key="media.id">
+                <h4>{{media.id}} , {{media.kind}}, {{media.type}} ,{{media.name}}, {{media.artist}}</h4>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import jquery from "jquery";
+import { mapGetters, mapActions } from "vuex";
 export default {
-    name: 'SearchPage',
+    name: "SearchPage",
     data() {
         return {
             searchField: "",
             searchSelect: "all",
             searchType: [
                 { text: "All", value: "all" },
+                { text: "Movie", value: "movie" },
                 { text: "Podcast", value: "podcast" },
-                { text: "Music", value: "musicote" },
+                { text: "Music", value: "song" },
                 { text: "Music Video", value: "musicVideo" },
-                { text: "Audio Book", value: "audiobook" }
+                { text: "Audio Book", value: "audiobook" },
             ],
-            searchResult: [],
             template: {
+                id: "",
+                kind: "",
                 imageCover: "",
                 name: "",
                 artist: "",
                 price: "",
             },
-            generatedURL: ""
-        }
+            generatedURL: "",
+        };
     },
     methods: {
-        async getSearchResult() {
-            jquery.getJSON(this.getQueryURL(), function(data) {
-                for (var i = 0; i < data.resultCount; i++) {
-                    console.log("Data: " + i + " Name: " + data.results[i].trackName + " Artist: " + data.results[i].artistName);
-                }
-            }).fail(function() {
-                console.log("An error occurred, could not reach host ");
-            });
+        ...mapActions(["addMedia", "clearSearchResults"]),
+        getSearchResult() {
+            this.clearSearchResults();
+            var dataResults;
+            jquery
+                .getJSON(this.getQueryURL(), function(data) {
+                    dataResults = data;
+                })
+                .done(() => {
+                    this.saveSearchResult(dataResults)
+                })
+                .fail(function() {
+                    console.log("An error occurred, could not reach host ");
+                });
         },
         getQueryURL() {
-            this.generatedURL = "https://itunes.apple.com/search?term=" + this.searchField;
+            this.generatedURL =
+                "https://itunes.apple.com/search?term=" + this.searchField;
             if (this.searchSelect != "all") {
-                this.generatedURL += "&media=" + this.searchSelect;
+                this.generatedURL += "&entity=" + this.searchSelect;
             }
             console.log("URL: " + this.generatedURL);
             return this.generatedURL;
+        },
+        saveSearchResult(data) {
+            for (var i = 0; i < data.resultCount; i++) {
+                this.addMedia({
+                    id: data.results[i].trackId,
+                    kind: data.results[i].kind,
+                    type: data.results[i].wrapperType,
+                    imageCover: data.results[i].artworkUrl100,
+                    name: data.results[i].trackName,
+                    artist: data.results[i].artistName,
+                });
+            }
         }
-    }
-}
+    },
+    computed: {
+        ...mapGetters(["getSearchResults"]),
+        searchResults() {
+            return this.getSearchResults;
+        },
+    },
+};
 </script>
