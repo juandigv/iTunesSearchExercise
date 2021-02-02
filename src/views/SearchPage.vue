661a45c5348961a4724a3f8a203c3d2001d17f8c
juandigv69 <juandigv69@gmail.com>
2021-02-02 03:27:35 -0400

    Improvements to the listing method
    Depending if the track is part of a collections, it shows the collection name

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -4,19 +4,29 @@
             <h1>iTunes Search & Selector</h1>
             <input v-model="searchField" placeholder="Search" />
             <select v-model="searchSelect">
-                                  <option
-                                    v-for="type in this.searchType"
-                                    :key="type.value"
-                                    :value="type.value"
-                                  >
-                                    {{ type.text }}
-                                  </option>
-                                </select>
+                                        <option
+                                          v-for="type in this.searchType"
+                                          :key="type.value"
+                                          :value="type.value"
+                                        >
+                                          {{ type.text }}
+                                        </option>
+                                      </select>
             <button v-on:click="getSearchResult">Search</button>
         </div>
         <div class="searchList">
             <div class="media" v-for="media in searchResults" :key="media.id">
-                <h4>{{media.id}} , {{media.kind}}, {{media.type}} ,{{media.name}}, {{media.artist}}</h4>
+                <div class="cardTemplate">
+                    <h2> {{media.type}}</h2>
+                    <img :src="media.artwork" onerror="this.src='https://apprecs.org/ios/images/app-icons/256/3d/1090617661.jpg'" />
+                    <div class="textContent">
+                        <h2>{{ media.name }}</h2>
+                        <h2 v-if="media.type != 'collection' || media.type != 'audiobook'">{{ media.collectionName}}</h2>
+                        <h4>{{ media.artist }}</h4>
+                        <h4 v-if="media.trackCount >1">{{ media.trackCount }} Tracks</h4>
+                        <h4>${{ media.price }}</h4>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
@@ -35,14 +45,14 @@ export default {
                 { text: "All", value: "all" },
                 { text: "Movie", value: "movie" },
                 { text: "Podcast", value: "podcast" },
-                { text: "Music", value: "song" },
+                { text: "Music", value: "album" },
                 { text: "Music Video", value: "musicVideo" },
                 { text: "Audio Book", value: "audiobook" },
             ],
             template: {
                 id: "",
                 kind: "",
-                imageCover: "",
+                artwork: "",
                 name: "",
                 artist: "",
                 price: "",
@@ -60,7 +70,7 @@ export default {
                     dataResults = data;
                 })
                 .done(() => {
-                    this.saveSearchResult(dataResults)
+                    this.saveSearchResult(dataResults);
                 })
                 .fail(function() {
                     console.log("An error occurred, could not reach host ");
@@ -77,16 +87,42 @@ export default {
         },
         saveSearchResult(data) {
             for (var i = 0; i < data.resultCount; i++) {
-                this.addMedia({
-                    id: data.results[i].trackId,
-                    kind: data.results[i].kind,
-                    type: data.results[i].wrapperType,
-                    imageCover: data.results[i].artworkUrl100,
-                    name: data.results[i].trackName,
-                    artist: data.results[i].artistName,
-                });
+                if (data.results[i].wrapperType == "collection") {
+                    this.addMedia({
+                        id: data.results[i].collectionId,
+                        type: data.results[i].collectionType,
+                        artwork: data.results[i].artworkUrl100,
+                        name: data.results[i].collectionName,
+                        artist: data.results[i].artistName,
+                        price: data.results[i].collectionPrice,
+                        trackCount: data.results[i].trackCount
+                    });
+                } else if (data.results[i].wrapperType == "audiobook") {
+                    this.addMedia({
+                        id: data.results[i].collectionId,
+                        type: data.results[i].wrapperType,
+                        artwork: data.results[i].artworkUrl100,
+                        name: data.results[i].collectionName,
+                        artist: data.results[i].artistName,
+                        price: data.results[i].collectionPrice,
+                        trackCount: data.results[i].trackCount
+                    });
+                } else {
+
+                    this.addMedia({
+                        id: data.results[i].trackId,
+                        type: data.results[i].kind,
+                        artwork: data.results[i].artworkUrl100,
+                        name: data.results[i].trackName,
+                        collectionName: data.results[i].collectionName,
+                        artist: data.results[i].artistName,
+                        price: data.results[i].collectionPrice,
+                        trackCount: data.results[i].trackCount
+                    });
+
+                }
             }
-        }
+        },
     },
     computed: {
         ...mapGetters(["getSearchResults"]),
@@ -96,3 +132,4 @@ export default {
     },
 };
 </script>
+
