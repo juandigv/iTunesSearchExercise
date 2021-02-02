b8e32cc168ee5f90540f03103c5860d98a67ae8f
juandigv69 <juandigv69@gmail.com>
2021-02-01 20:18:08 -0400

    Added JQuery package to retrieve data using API URL

--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,54 +1,59 @@
 <template>
-  <div class="searchList">
-    <h1> iTunes Search & Selector </h1>
-    <input v-model="searchField" placeholder="Search"/>
-    <select v-model="searchSelect">
-      <option v-for="type in this.searchType" :key="type.value" :value="type.value"> {{type.text}} </option>
-    </select>
-    <button v-on:click="getSearchResult">
-      Search
-    </button>
-  </div>  
+    <div class="searchList">
+        <h1> iTunes Search & Selector </h1>
+        <input v-model="searchField" placeholder="Search" />
+        <select v-model="searchSelect">
+                                          <option v-for="type in this.searchType" :key="type.value" :value="type.value"> {{type.text}} </option>
+                                        </select>
+        <button v-on:click="getSearchResult">
+                                          Search
+                                        </button>
+    </div>
 </template>
 
 <script>
+import jquery from "jquery";
 export default {
-  name: 'SearchPage',
-  data(){
-    return{
-      searchField: "",
-      searchSelect: "all",
-      searchType:[
-        {text:"All",value:"all"},
-        {text:"Movie",value:"movie"},
-        {text:"Podcast",value:"podcast"},
-        {text:"Music",value:"musictrack"},
-        {text:"Music Video",value:"musicvideo"},
-        {text:"Audio Book",value:"audiobook"},
-        {text:"Shortfilm",value:"shortfilm"},
-        {text:"TV Show",value:"tvshow"},
-        {text:"Software",value:"software"},
-        {text:"E-Book",value:"ebook"},
-        ],
-      searchResult: [],
-      template: {
-        imageCover: "",
-        name: "",
-        artist: "",
-        price: "",
-      },
-      generatedURL: ""
+    name: 'SearchPage',
+    data() {
+        return {
+            searchField: "",
+            searchSelect: "all",
+            searchType: [
+                { text: "All", value: "all" },
+                { text: "Podcast", value: "podcast" },
+                { text: "Music", value: "musicote" },
+                { text: "Music Video", value: "musicVideo" },
+                { text: "Audio Book", value: "audiobook" }
+            ],
+            searchResult: [],
+            template: {
+                imageCover: "",
+                name: "",
+                artist: "",
+                price: "",
+            },
+            generatedURL: ""
+        }
+    },
+    methods: {
+        async getSearchResult() {
+            jquery.getJSON(this.getQueryURL(), function(data) {
+                for (var i = 0; i < data.resultCount; i++) {
+                    console.log("Data: " + i + " Name: " + data.results[i].trackName + " Artist: " + data.results[i].artistName);
+                }
+            }).fail(function() {
+                console.log("An error occurred, could not reach host ");
+            });
+        },
+        getQueryURL() {
+            this.generatedURL = "https://itunes.apple.com/search?term=" + this.searchField;
+            if (this.searchSelect != "all") {
+                this.generatedURL += "&media=" + this.searchSelect;
+            }
+            console.log("URL: " + this.generatedURL);
+            return this.generatedURL;
+        }
     }
-  },
-  methods: {
-    getSearchResult(){
-      console.log("Searching for "+ this.searchField +" in "+ this.searchSelect);
-      this.generatedURL = "https://itunes.apple.com/search?term="+this.searchField;
-      if(this.searchSelect != "all"){
-        this.generatedURL+="&media="+this.searchSelect;
-      }
-      console.log("URL: "+this.generatedURL);
-    }
-  }
 }
 </script>
